2986135b51bb00330438c41df6b7597e20537860
Tienou4 <tienouulrichbrayanne@gmail.com>
2025-02-24 05:21:11 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: ["./src/Index.html","./src/form.html"], // Ajoutez les chemins de vos fichiers HTML/JS
+    theme: {
+        extend: {},
+        },
+        plugins: [],
+}
